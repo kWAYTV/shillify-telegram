aa90a7f1f6708243a098ca59ab68f1ebebadf498
kWAY <55437151+kWAYTV@users.noreply.github.com>
2022-10-30 01:36:26 +0200

    Added group leaver with error handler  and adder more errors to joiner

--- b.py
+++ b.py
@@ -704,7 +704,6 @@ async def handle_new_message(event):
     if event.is_private:
         from_ = await event.client.get_entity(event.from_id)
         if not from_.bot:
-            print("Autoreplying...: " + time.asctime(), '-', event.message)
             slow_type(Fore.MAGENTA + f"Autoreplying...: {Fore.RESET} -  Time: {time.asctime()} - Event: {event.message}.", 0.00001)
             time.sleep(1)
             await event.respond(automessage)
